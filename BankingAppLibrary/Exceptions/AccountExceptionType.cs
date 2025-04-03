e24d25eb1cc061796ea780aa5f1f858dcbfdeb99
Ahmad Moaz <amoaz@my.centennialcollge.ca>
2025-04-03 09:59:37 -0400

    updated AccountExceptionType

--- BankingAppLibrary/Exceptions/AccountExceptionType.cs
+++ BankingAppLibrary/Exceptions/AccountExceptionType.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace BankingAppLibrary.Exceptions
 {
-    internal class AccountExceptionType
+    public enum AccountExceptionType
     {
+        ACCOUNT_DOES_NOT_EXIST,
+        ACCOUNT_ALREADY_EXIST,
+        CREDIT_LIMIT_HAS_BEEN_EXCEEDED,
+        INSUFFICIENT_FUNDS,
+        NAME_NOT_ASSOCIATED_WITH_ACCOUNT,
+        NO_OVERDRAFT_FOR_THIS_ACCOUNT,
+        PASSWORD_INCORRECT,
+        USER_DOES_NOT_EXIST,
+        USER_ALREADY_EXIST,
+        USER_NOT_LOGGED_IN
     }
 }
