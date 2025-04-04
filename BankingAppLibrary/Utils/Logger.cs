f3f45579af27f49168bd45cfc45b6f14a9d377e7
Ahmad Moaz <amoaz@my.centennialcollge.ca>
2025-04-03 22:01:56 -0400

    Updated logger.cs

--- BankingAppLibrary/Utils/Logger.cs
+++ BankingAppLibrary/Utils/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BankingAppLibrary.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace BankingAppLibrary.Utils
 {
-    internal class Logger
+    public static class Logger
     {
+        private static List<string> loginEvents = new List<string>();
+        private static List<string> transactionEvents = new List<string>();
+
+        public static void LoginHandler(object sender, LoginEventArgs args)
+        {
+            string entry = $"{args.PersonName} {(args.Success ? "successfully" : "unsuccessfully")} {args.EventType} on {args.Time}";
+            loginEvents.Add(entry);
+        }
+
+        public static void TransactionHandler(object sender, TransactionEventArgs args)
+        {
+            string operation = args.Amount > 0 ? "Deposit" : "Withdraw";
+            string entry = $"{args.PersonName} {operation} {Math.Abs(args.Amount):C} {(args.Success ? "successfully" : "unsuccessfully")}";
+            transactionEvents.Add(entry);
+        }
+
+        public static void DisplayLoginEvents()
+        {
+            int i = 1;
+            foreach (string e in loginEvents)
+            {
+                Console.WriteLine($"{i++}. {e}");
+            }
+        }
+
+        public static void DisplayTransactionEvents()
+        {
+            int i = 1;
+            foreach (string e in transactionEvents)
+            {
+                Console.WriteLine($"{i++}. {e}");
+            }
+        }
     }
 }
